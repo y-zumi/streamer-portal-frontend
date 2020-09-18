8a1d6502b708d0209a9627f64c22ded6cd7c7cbc
Kohei ARAI <kokoheia@mercari.com>
2020-09-18 17:37:22 +0900

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import GlobalHeader from './GlobalHeader/GlobalHeader.tsx';
 import {StreamingSignItem, StreamingMedia} from './StreamingSignItem/StreamingSignItem.tsx';
 
 function App() {
   return (
     <div className="App">
-        <h2>加藤純一さんの配信</h2>
+        <GlobalHeader />
+        <h2 id='title'>加藤純一さんの配信</h2>
         <StreamingSignItem media={StreamingMedia.youtube} isStreaming={true} />
         <StreamingSignItem media={StreamingMedia.twitch} isStreaming={false} />
         <StreamingSignItem media={StreamingMedia.niconico} isStreaming={false} />
